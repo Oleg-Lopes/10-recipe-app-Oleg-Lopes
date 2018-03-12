33f62b2fb4874317dcb5137676e83c54a996e74f
Oleg-Lopes <oleg.lopes@chasacademy.se>
2018-03-12 14:04:38 +0100

    laravel setup, auth, recipes table, cors

---
+++ database/migrations/2018_03_12_120828_create_recipes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('label');
+            $table->text('ingredients');
+            $table->text('health_labels');
+            $table->integer('calories');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
